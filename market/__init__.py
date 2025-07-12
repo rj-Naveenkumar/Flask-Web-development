dd257d15bbb7f28a01ea17c672380e9caf6f7737
naveen <rjnaveenkumar2004@gmail.com>
2025-07-12 22:22:19 +0530

    restructuring

--- market/__init__.py
+++ market/__init__.py
@@ -2,12 +2,12 @@ from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy    #sqlite
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///market.db'
-
-
-import models
-import routes
+db=SQLAlchemy(app)  #initialize instance for sql
 
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+
+
+
+
--- market/models.py
+++ market/models.py
@@ -1,5 +1,3 @@
-db=SQLAlchemy(app)  #initialize instance for sql
-
 class Item(db.Model):
     id=db.Column(db.Integer(), primary_key=True)
     name=db.Column(db.String(length=30), nullable=False, unique=True)
