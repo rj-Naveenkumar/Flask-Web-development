678502f8c3e0012322e94b6889c12605e5e28051
naveen <rjnaveenkumar2004@gmail.com>
2025-07-13 11:21:38 +0530

    registration forms

--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,9 @@
 from market import app
-from flask import render_template
+from flask import render_template,redirect,url_for
 from market.models import Item
+from market.models import User,Item
 from market.forms import RegisterForm
+from market import db
 
 @app.route('/')
 @app.route('/home')
@@ -13,11 +15,16 @@ def market_page():
     items = Item.query.all()
     return render_template('market.html',items=items)
 
-@app.route('/register')
+@app.route('/register' ,methods=['GET','POST'])
 def register_page():
     form = RegisterForm()
     if form.validate_on_submit():
-        
+        user_to_create=User(username=form.username.data,
+                             email_address=form.email_address.data,
+                            password_hash=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page')) 
 
 
     return render_template('register.html',form=form)
